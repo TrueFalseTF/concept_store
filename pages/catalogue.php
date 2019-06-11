a47a42cc0603dd4d408641aab1d37911c4697d85
TrueFalseTF <dimon_mcensk@mail.ru>
2019-06-11 03:14:36 +0300

    Построена черновая архитектура магазина

---
+++ pages/catalogue.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">    
+    <title>Магазин</title>
+    <link rel="stylesheet" href="./css/shopping_basket_styles.css">
+    <script src="./pages/js/catalogue.js"></script>
+    <script src="./pages/js/shopping_basket.js"></script>
+</head>
+<body>
+<h2>Каталог магазина</h2>
+<!--таблица товаров-->
+<p></p>
+
+<!--банер корзины-->
+<a href="./pages/shopping_basket.php">Корзина</a>
+    
+</body>
+</html>
---
+++ pages/shopping_basket.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">    
+    <title>Магазин</title>
+    <link rel="stylesheet" href="./css/shopping_basket_styles.css">
+    <script src="./pages/js/jsshopping_basket.js"></script>
+</head>
+<body>
+    <?php
+        echo("shopping_basket");
+    ?>
+    
+</body>
+</html>
