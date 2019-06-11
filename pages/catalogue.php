5ba04cc5825db64d2aeb16e567a9638ac322e954
TrueFalseTF <dimon_mcensk@mail.ru>
2019-06-11 05:00:54 +0300

    Черновое редактирование скелета

--- pages/catalogue.php
+++ pages/catalogue.php
@@ -2,18 +2,22 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">    
-    <title>Магазин</title>
+    <title>Магазин</title>    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/shopping_basket_styles.css">
     <script src="./pages/js/catalogue.js"></script>
     <script src="./pages/js/shopping_basket.js"></script>
 </head>
 <body>
-<h2>Каталог магазина</h2>
-<!--таблица товаров-->
-<p></p>
+    <h2>Каталог магазина</h2>
+    <!--product table-->
+    <div class="product table">
+        <!--таблица-->
+        <em></em><p></p><p></p>
+    </div>    
 
-<!--банер корзины-->
-<a href="./pages/shopping_basket.php">Корзина</a>
+    <!--банер корзины-->
+    <a href="./pages/shopping_basket.php">Корзина</a>
     
 </body>
 </html>
